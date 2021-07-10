7efabaa295f07d40e145bdf8512035335c1c0f65
neketka <neketka@gmail.com>
2021-07-09 20:45:55 -0400

    Changed entity loading scheme.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,38 +7,23 @@ import { AdminModule } from './admin/admin.module';
 import { LeadersModule } from './leaders/leaders.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './model/user.entity';
-import { Challenge } from './model/challenge.entity';
-import { EventBase } from './model/event-base.entity';
-import { EventProgress } from './model/event-progress.entity';
-import { GroupMember } from './model/group-member.entity';
-import { Group } from './model/group.entity';
-import { PrevChallenge } from './model/prev-challenge.entity';
-import { EventReward } from './model/event-reward.entity';
-import { SessionLogEntry } from './model/session-log-entry.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.CORNELLGODB_HOST,
       username: process.env.CORNELLGODB_USER,
       password: process.env.CORNELLGODB_PASS,
       database: process.env.CORNELLGODB_DBNAME,
-      schema: process.env.CORNELLGODB_SCHEMA,
+      schema: 'public',
       logNotifications: true,
       cache: true,
-      entities: [
-        User,
-        Group,
-        GroupMember,
-        Challenge,
-        PrevChallenge,
-        EventBase,
-        EventReward,
-        EventProgress,
-        SessionLogEntry,
-      ],
+      entities: ['dist/model/*.entity{.ts,.js}'],
+      migrations: ['dist/migration/*{.ts,.js}'],
+      migrationsRun: true,
     }),
     EventsModule,
     GroupsModule,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
