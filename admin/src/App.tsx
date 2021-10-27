598601c588c310eb4acc4d6a65c18cac4cbaf79e
Eric Huang <erichuanglm@gmail.com>
2021-10-27 13:43:15 -0400

    add appbar

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
 export function App() {
-  return <div>Hello world!</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography
+          component="h1"
+          variant="h4"
+          color="inherit">
+          CornellGO!Manager
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
