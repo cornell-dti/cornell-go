14c9dffdda60e599a8b2b802ea5d894472ae40fc
neketka <neketka@gmail.com>
2021-10-26 23:10:46 -0400

    Attempted to fix global installation

--- ci/admin-build/run.sh
+++ ci/admin-build/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEBIAN_FRONTEND=noninteractive
+npm install -g prettier nest jest rimraf
 cd admin
 npm install
 npm run build
--- ci/server-prettier/run.sh
+++ ci/server-prettier/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEBIAN_FRONTEND=noninteractive
+npm install -g prettier nest jest rimraf
 cd server
 npm install
 npm run checkformat
