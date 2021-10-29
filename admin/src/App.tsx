cf22961e96dcb8755c9a57a190e80e94c4dce47e
Eric Huang <erichuanglm@gmail.com>
2021-10-29 15:58:52 -0400

    add sidebar and tabable pagees

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import Sidebar from './Sidebar';
 
 export function App() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography
-          component="h1"
-          variant="h4"
-          color="inherit">
-          CornellGO!Manager
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            component="h1"
+            variant="h4"
+            color="inherit">
+            CornellGO!Manager
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Sidebar />
+    </div>
   );
 }
