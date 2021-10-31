f5159b0bab7e8c7b37ccb80e775e864083ecc159
neketka <neketka@gmail.com>
2021-10-31 19:58:30 -0400

    Added CI for this repository (#6)
    * Started working on Heroku CI/CD
    
    * Added github actions YML files for docker CI
    
    * Added big diff warning (borrowed from Courseplan)
    
    * Fixed Dockerfiles
    
    * Changed location of Dockerfile for Heroku quirk
    
    * Fixed yml files
    
    * Changed executable permissions
    
    * Fixed yml docker typo
    
    * Fixed other typos
    
    * Fixed sh script bugs
    
    * Changed container type
    
    * Fixed trailing comma in package.json
    
    * Fixed some issues
    
    * Attempted to fix global installation
    
    * Fixed installation
    
    * Formatted all documents
    
    * Added yarn
    
    * Removed yarn
    
    * pin node version to v14
    
    Co-authored-by: Enoch Chen <sirenochen@gmail.com>

---
+++ cd/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DEBIAN_FRONTEND=noninteractive
+npm install -g prettier @nestjs/cli jest rimraf
+cd server
+npm install
+npm run build
+cd ../admin
+npm install
+npm run build
+cd ..
