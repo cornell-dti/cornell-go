57027f948f33d6113bc74f9d80e7befaab4e5c23
bd324 <bd324@cornell.edu>
2021-11-17 16:57:33 -0500

    add apple authen

--- server/src/auth/apple/apple.controller.ts
+++ server/src/auth/apple/apple.controller.ts
@@ -1,6 +1,23 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthType } from 'src/model/user.entity';
 import { AuthService } from '../auth.service';
+import { TokenDto } from '../constant';
 
 @Controller('apple')
-export class AppleController {}
+export class AppleController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  async login(@Body() req: TokenDto) {
+    const success: string = await this.authService.login(
+      req.token,
+      AuthType.APPLE,
+    );
+    // success is a string, either"login success" or "verify error"
+    return success;
+  }
+}
+
+/**Useful sources:
+ * https://dev.to/heyitsarpit/how-to-add-signin-with-apple-on-your-website-43m9 */
