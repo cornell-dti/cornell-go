c6426f2e5db25bcfeb34b1e5ada02231d7efc8e3
Nirbhay Singh Narang <nsn8@cornell.edu>
2022-02-17 16:21:20 -0500

    Added LeaderBoard Cells.
    Implemented Leaderboard cells. Populated screen with dummy data.

--- game/lib/leaderboard/leaderboard_widget.dart
+++ game/lib/leaderboard/leaderboard_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:game/Widgets/back_btn.dart';
+import 'package:game/Widgets/leaderboard_cell.dart';
 
 class LeaderboardWidget extends StatefulWidget {
   LeaderboardWidget({Key? key}) : super(key: key);
@@ -14,10 +15,24 @@ class _LeaderboardWidgetState extends State<LeaderboardWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: scaffoldKey,
-        floatingActionButton: backBtn(scaffoldKey, context),
-        floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-        backgroundColor: Color(0xFFDBE2E7),
-        body: Center(child: Text("TODO")));
+      key: scaffoldKey,
+      floatingActionButton: backBtn(scaffoldKey, context),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 150),
+        child: Container(
+          child: ListView(
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            children: [
+              leaderBoardCell(context, "Nirbhay", "1", "23", true),
+              leaderBoardCell(context, "Nirbhay 2", "2", "20", false),
+              leaderBoardCell(context, "Nirbhay 3", "3", "19", false)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
