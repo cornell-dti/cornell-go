abef29a359efe521c4e20cd52f031650a25c22fb
Nirbhay Singh Narang <nsn8@cornell.edu>
2022-02-17 17:20:44 -0500

    fixed icon error, changed FAB position.
    Fixed error for 'bolt_rounded' not found. Changed position of FAB button.

--- game/lib/Widgets/back_btn.dart
+++ game/lib/Widgets/back_btn.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 
 Widget backBtn(scaffoldKey, context) {
   Color Carnelian = Color(0xFFB31B1B);
-  return Container(
-    margin: EdgeInsets.only(top: 75, left: 25),
-    child: FloatingActionButton(
-      onPressed: () {
-        Navigator.pop(context);
-      },
-      backgroundColor: Carnelian,
-      child: const Icon(Icons.arrow_back),
+  return Align(
+    alignment: Alignment.topLeft,
+    child: Padding(
+      padding: const EdgeInsets.only(left: 8.0),
+      child: Container(
+        margin: EdgeInsets.only(top: 75, left: 25),
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          backgroundColor: Carnelian,
+          child: const Icon(Icons.arrow_back),
+        ),
+      ),
     ),
   );
 }
