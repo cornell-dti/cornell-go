b3c6f0b6c4d311ab81a968460f7fc6a049966b51
neketka <neketka@gmail.com>
2022-03-03 17:52:34 -0500

    Added leaders.

--- game/lib/model/event_model.dart
+++ game/lib/model/event_model.dart
@@ -4,6 +4,9 @@ import 'package:game/api/game_client_dto.dart';
 
 class EventModel extends ChangeNotifier {
   Map<String, UpdateEventDataEventDto> _events = {};
+  Map<String, int> _topPlayerLength = {};
+  Map<String, List<UpdateLeaderDataUserDto>> _topPlayers = {};
+
   ApiClient _client;
   List<UpdateEventDataEventDto>? searchResults;
 
@@ -17,6 +20,50 @@ class EventModel extends ChangeNotifier {
       });
       notifyListeners();
     });
+
+    client.clientApi.updateLeaderDataStream.listen((event) {
+      final players = _topPlayers[event.eventId];
+      for (int i = event.offset; i < event.users.length; i++) {
+        if (i > _topPlayers.length) {
+          players?.add(event.users[i - event.users.length]);
+        } else {
+          players?[i] = event.users[i - event.users.length];
+        }
+      }
+      int diff = _topPlayerLength.length - (players?.length ?? 0);
+      if (diff > 0) {
+        client.serverApi
+            ?.requestEventLeaderData(players?.length ?? 0, diff, event.eventId);
+      }
+      notifyListeners();
+    });
+  }
+
+  List<UpdateLeaderDataUserDto> getTopPlayersForEvent(
+      String eventId, int count) {
+    final topPlayers = _topPlayers[eventId];
+    if (topPlayers != null) {
+      final toLoad = _topPlayerLength[eventId]!;
+      if (count > toLoad) {
+        _topPlayerLength[eventId] = count;
+        _client.serverApi
+            ?.requestEventLeaderData(toLoad, toLoad - count, eventId);
+      }
+      return topPlayers;
+    } else {
+      _topPlayers[eventId] = [];
+      _topPlayerLength[eventId] = 0;
+      _client.serverApi?.requestEventLeaderData(0, count, eventId);
+      return [];
+    }
+  }
+
+  bool isLoadingPlayers(String eventId) {
+    if (_topPlayers.containsKey(eventId)) {
+      return _topPlayers[eventId]?.length != _topPlayerLength[eventId];
+    } else {
+      return true;
+    }
   }
 
   UpdateEventDataEventDto? getEventById(String id) {
