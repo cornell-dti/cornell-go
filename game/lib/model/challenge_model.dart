6e36f6becb401de42b8d0a438039c44e2e119d98
neketka <neketka@gmail.com>
2022-03-03 19:50:17 -0500

    Added getting challenge by id.

--- game/lib/model/challenge_model.dart
+++ game/lib/model/challenge_model.dart
@@ -4,13 +4,22 @@ import 'package:game/api/game_client_dto.dart';
 
 class ChallengeModel extends ChangeNotifier {
   Map<String, UpdateChallengeDataChallengeDto> _challengesById = {};
+  ApiClient _client;
 
-  ChallengeModel(ApiClient client) {
+  ChallengeModel(ApiClient client) : _client = client {
     client.clientApi.updateChallengeDataStream.listen((event) {
       event.challenges.forEach((element) {
         _challengesById[element.id] = element;
       });
       notifyListeners();
     });
+  }
+
+  UpdateChallengeDataChallengeDto? getChallengeById(String id) {
+    final chal = _challengesById[id];
+    if (chal == null) {
+      _client.serverApi?.requestChallengeData([id]);
+    }
+    return chal;
   }
 }
