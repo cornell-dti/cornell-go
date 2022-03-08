7457a1e31d9916358231b34f2b4117afc9056958
cli66 <cathrynli606@gmail.com>
2022-03-08 17:44:33 -0500

    Fixing issue with branching/formatting

--- server/src/group/group.service.ts
+++ server/src/group/group.service.ts
@@ -19,7 +19,7 @@ export class GroupService {
     private groupsRepository: EntityRepository<Group>,
     @InjectRepository(GroupMember)
     private groupMembersRepository: EntityRepository<GroupMember>,
-  ) {}
+  ) { }
 
   /** Creates a group from an event */
   async createFromEvent(event: EventBase, host: User) {
@@ -52,12 +52,13 @@ export class GroupService {
     return groupMember!.group.load();
   }
 
-  /** Get group from the id.
+  /** Get group from the id. 
    * Throws a NotFoundError if the id does not correspond to a group. */
   async getGroupFromID(id: string): Promise<Group> {
-    const group = await this.groupsRepository.findOne({ friendlyId: id });
-    if (group == null) throw NotFoundError;
-    return group;
+    const group = await this.groupsRepository.findOne({ friendlyId: id })
+    if (group == null)
+      throw NotFoundError
+    return group
   }
 
   /** Helper function that removes a user from their current group.
@@ -89,9 +90,7 @@ export class GroupService {
   /** Moves the user out of their current group into a new group */
   async leaveGroup(user: User) {
     const oldGroup = await this.getGroupForUser(user);
-    const newGroupId = (
-      await this.createFromEvent(await oldGroup.currentEvent.load(), user)
-    ).friendlyId;
-    this.joinGroup(user, newGroupId);
+    const newGroupId = (await this.createFromEvent((await oldGroup.currentEvent.load()), user)).friendlyId;
+    this.joinGroup(user, newGroupId)
   }
 }
