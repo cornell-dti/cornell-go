d82aeb79801fb5769218dd3eb1fb9b97a3abe8ea
cli66 <cathrynli606@gmail.com>
2022-03-08 17:14:18 -0500

    Implemented group.gateway.joinGroup, leaveGroup.

--- server/src/group/group.service.ts
+++ server/src/group/group.service.ts
@@ -8,6 +8,7 @@ import { UserService } from '../user/user.service';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityRepository } from '@mikro-orm/postgresql';
 import { v4 } from 'uuid';
+import { elementAt, NotFoundError } from 'rxjs';
 
 @Injectable()
 export class GroupService {
@@ -18,7 +19,7 @@ export class GroupService {
     private groupsRepository: EntityRepository<Group>,
     @InjectRepository(GroupMember)
     private groupMembersRepository: EntityRepository<GroupMember>,
-  ) {}
+  ) { }
 
   /** Creates a group from an event */
   async createFromEvent(event: EventBase, host: User) {
@@ -49,5 +50,47 @@ export class GroupService {
   async getGroupForUser(user: User): Promise<Group> {
     const groupMember = await user.groupMember!.load();
     return groupMember!.group.load();
+  }
+
+  /** Get group from the id. 
+   * Throws a NotFoundError if the id does not correspond to a group. */
+  async getGroupFromID(id: string): Promise<Group> {
+    const group = await this.groupsRepository.findOne({ friendlyId: id })
+    if (group == null)
+      throw NotFoundError
+    return group
+  }
+
+  /** Helper function that removes a user from their current group.
+   * If the user is the only member, deletes the group. */
+  async removeUserFromGroup(user: User) {
+    const oldGroup = await this.getGroupForUser(user);
+    oldGroup.members.remove(await user.groupMember!.load());
+    if (oldGroup.members.length == 0)
+      this.groupsRepository.removeAndFlush(oldGroup);
+  }
+
+  /** Adds user to an existing group, given by the group's id. */
+  async joinGroup(user: User, joinId: string) {
+    this.removeUserFromGroup(user);
+    const group = await this.getGroupFromID(joinId);
+    let groupMember: GroupMember = this.groupMembersRepository.create({
+      isHost: false,
+      user: user,
+      group: group,
+    });
+
+    group.members.set([groupMember]);
+    user.groupMember?.set(groupMember);
+    groupMember.user.set(user);
+
+    await this.groupsRepository.persistAndFlush(group);
+  }
+
+  /** Moves the user out of their current group into a new group */
+  async leaveGroup(user: User) {
+    const oldGroup = await this.getGroupForUser(user);
+    const newGroupId = (await this.createFromEvent((await oldGroup.currentEvent.load()), user)).friendlyId;
+    this.joinGroup(user, newGroupId)
   }
 }
