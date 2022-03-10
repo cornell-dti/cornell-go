8ae3bb406ea09cc88e42dbb6e807269f7597f0c8
neketka <neketka@gmail.com>
2022-03-10 15:05:55 -0500

    Admin gateway methods (#39)
    * Added admin callbacks.
    
    * Added client callbacks.
    
    * Added administration example.
    
    * Added admin access requester.
    
    * Fixed build errors.
    
    * Fixed dependency issue.
    
    * Added deletion ability.

---
+++ server/src/admin/admin.service.ts
@@ -0,0 +1,44 @@
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { EntityRepository } from '@mikro-orm/postgresql';
+import { Injectable } from '@nestjs/common';
+import { Challenge } from 'src/model/challenge.entity';
+import { EventBase } from 'src/model/event-base.entity';
+import { EventReward } from 'src/model/event-reward.entity';
+import { User } from 'src/model/user.entity';
+
+@Injectable()
+export class AdminService {
+  constructor(
+    @InjectRepository(User) private userRepository: EntityRepository<User>,
+    @InjectRepository(EventReward)
+    private rewardRepository: EntityRepository<EventReward>,
+    @InjectRepository(EventBase)
+    private eventRepository: EntityRepository<EventBase>,
+    @InjectRepository(Challenge)
+    private challengeRepository: EntityRepository<Challenge>,
+  ) {}
+
+  async requestAdminAccess(adminId: string) {
+    const admin = await this.userRepository.findOne({ id: adminId });
+    if (admin) {
+      admin.adminRequested = true;
+      await this.userRepository.persistAndFlush(admin);
+    }
+  }
+
+  async setAdminStatus(adminId: string, granted: boolean) {
+    const admin = await this.userRepository.findOne({ id: adminId });
+    if (admin) {
+      admin.adminGranted = granted;
+      admin.adminRequested = false;
+      await this.userRepository.persistAndFlush(admin);
+    }
+    return admin;
+  }
+
+  async getAllRequestingAdmins() {
+    return await this.userRepository.find({
+      adminRequested: true,
+    });
+  }
+}
