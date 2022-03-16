2f7f604e5a31f7b6dfc5f5f6a54fb406b379a5de
neketka <neketka@gmail.com>
2022-03-15 21:01:51 -0400

    Added intro text.

--- admin/src/components/Home.tsx
+++ admin/src/components/Home.tsx
@@ -4,18 +4,11 @@ export function Home() {
   return (
     <>
       <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-        non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-        tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non
-        tellus. Convallis convallis tellus id interdum velit laoreet id donec
-        ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
-        suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
-        quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet
-        proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
-        tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
-        varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
-        Lorem donec massa sapien faucibus et molestie ac.
+        Welcome to the CornellGO! Manager! If you need to approve any admins,
+        click on the Admin Approval tab. To start, click on the Events tab.
+        Create, edit, and select events there. If you select an event, you can
+        click on Challenges and Rewards to create and edit challenges and
+        rewards related to the selected event.
       </Typography>
     </>
   );
