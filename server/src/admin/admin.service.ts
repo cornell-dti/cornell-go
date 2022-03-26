c92386f6a65d9f9286834952605f30b31ea20687
cathli66 <cathrynli606@gmail.com>
2022-03-26 17:22:47 -0400

    Implemented requestRewards, updateRewards in admin.gateway.

--- server/src/admin/admin.service.ts
+++ server/src/admin/admin.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from '@nestjs/common';
 import { Challenge } from 'src/model/challenge.entity';
 import { EventBase } from 'src/model/event-base.entity';
 import { EventReward } from 'src/model/event-reward.entity';
+import { RewardDto } from './update-rewards.dto';
 import { User } from 'src/model/user.entity';
+import { v4 } from 'uuid';
 
 @Injectable()
 export class AdminService {
@@ -40,5 +42,64 @@ export class AdminService {
     return await this.userRepository.find({
       adminRequested: true,
     });
+  }
+
+  /** Get rewards of the user */
+  async getRewardsForUser(user: User): Promise<EventReward[]> {
+    return user.rewards.getItems();
+  }
+
+  /** Deletes all rewards with IDs listed in removeIds.
+   * Does nothing if the reward's ID is not in the user's rewards. */
+  async deleteRewards(removeIds: string[]) {
+    for (const i in removeIds) {
+      const reward = this.rewardRepository.findOneOrFail({ id: removeIds[i] });
+      this.rewardRepository.removeAndFlush(reward);
+    }
+  }
+
+  /** Creates a EventReward given RewardDto reward */
+  async createFromUpdateDTO(reward: RewardDto): Promise<EventReward> {
+    const event_reward = this.rewardRepository.create({
+      id: reward.id,
+      containingEvent: await this.eventRepository.findOneOrFail({
+        id: reward.containingEventId,
+      }),
+      rewardDescription: reward.description,
+      rewardRedeemInfo: reward.redeemInfo,
+      isRedeemed: false,
+    });
+    await this.rewardRepository.persistAndFlush(event_reward);
+    return event_reward;
+  }
+
+  /** Updates the repository with all rewards listed in rewards.
+   * Adds a new reward if it does not exist, otherwise overwrites the
+   * old reward. */
+  async updateRewards(
+    rewards: RewardDto[],
+  ): Promise<[EventBase[], EventBase[]]> {
+    var newEvents = Array();
+    var oldEvents = Array();
+
+    rewards.forEach(async reward => {
+      if (reward.id === '') {
+        reward.id = v4();
+      }
+      const eventReward = await this.createFromUpdateDTO(reward);
+      const oldReward = await this.rewardRepository.findOne({
+        id: eventReward.id,
+      });
+      if (oldReward === null) {
+        newEvents.push(eventReward.containingEvent.getEntity());
+      } else if (
+        oldReward.containingEvent.id != eventReward.containingEvent.id
+      ) {
+        newEvents.push(eventReward.containingEvent.getEntity());
+        oldEvents.push(oldReward.containingEvent.getEntity());
+      }
+      this.rewardRepository.persistAndFlush(eventReward);
+    });
+    return [oldEvents, newEvents];
   }
 }
