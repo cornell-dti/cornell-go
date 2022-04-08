c7063ea9e86984766329755538c0339deea3c891
Brian La <brianyla@gmail.com>
2022-04-08 15:25:23 -0400

    Initial RewardsWidget

---
+++ game/lib/rewards/rewards_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:game/widget/back_btn.dart';
+
+class RewardsWidget extends StatefulWidget {
+  RewardsWidget({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _RewardsWidget();
+
+}
+
+class _RewardsWidget extends State<RewardsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
