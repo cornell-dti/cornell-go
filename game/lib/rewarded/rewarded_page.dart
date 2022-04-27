ef6f8d21f8d9d189ce7a12d11bc318fb5f0c3d9d
youssef-attia <youssef.attia.ya@gmail.com>
2022-04-27 18:21:53 -0400

    Added some more design elements

--- game/lib/rewarded/rewarded_page.dart
+++ game/lib/rewarded/rewarded_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:game/model/game_model.dart';
+import 'dart:ui';
 
 class RewardWidget extends StatefulWidget {
   const RewardWidget({Key? key, required this.homePageState}) : super(key: key);
@@ -14,50 +15,64 @@ class _RewardWidgetState extends State<RewardWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Color.fromARGB(255, 165, 165, 165),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Congrats!',
-              style: TextStyle(fontSize: 80),
+        key: scaffoldKey,
+        body: Center(
+            child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(widget.homePageState.imageUrl),
+              fit: BoxFit.cover,
             ),
-            Icon(
-              Icons.star,
-              color: Colors.white,
-              size: 120.0,
-              semanticLabel: 'Text to announce in accessibility modes',
-            ),
-            const Text(
-              'You have found',
-              style: TextStyle(fontSize: 40),
-            ),
-            Text(
-              widget.homePageState.name,
-              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              '+1!',
-              style: const TextStyle(fontSize: 40),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text(
-                'Keep Searching',
-                style: TextStyle(fontSize: 30),
+          ),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text('Congrats!',
+                      style: TextStyle(
+                        fontSize: 80,
+                        color: Colors.white,
+                      )),
+                  const Icon(
+                    Icons.star,
+                    color: Colors.white,
+                    size: 120.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  ),
+                  const Text(
+                    'You have found',
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                  Text(
+                    widget.homePageState.name,
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  const Text(
+                    '+1!',
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      'Keep Searching',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      primary: Colors.black,
+                    ),
+                  ),
+                ],
               ),
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.red,
-                primary: Colors.black,
-              ),
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        )));
   }
 }
