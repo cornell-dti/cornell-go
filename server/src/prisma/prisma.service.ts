30cbc0afc12dcebff2cbacda148bca3d78874df2
Chris Gu <chrisgu12321@gmail.com>
2022-11-15 18:47:48 -0500

    done

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -3,10 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({ errorFormat: 'minimal' });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
