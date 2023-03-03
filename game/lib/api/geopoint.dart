a19c3a685271708d1e14215ce493e569564fed63
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-03-03 17:07:11 -0500

    Update Flutter API (#101)
    * Made changes to Flutter packages and deployment versions.
    
    * Add location request bug fix.
    
    * Run formatter + fix backend bug.

--- game/lib/api/geopoint.dart
+++ game/lib/api/geopoint.dart
@@ -1,18 +1,22 @@
 import 'package:geolocator/geolocator.dart';
 
 class GeoPoint {
+  static bool didMakeRequest = false;
   double _lat = 0;
   double _long = 0;
 
   double get lat => _lat;
   double get long => _long;
 
+  static bool _isRequestingLocationPermissions = false;
+  static bool _isRequestingLocation = false;
+
   GeoPoint(double lat, double long) {
     _lat = lat;
     _long = long;
   }
 
-  static Future<GeoPoint> current() async {
+  static Future<GeoPoint?> current() async {
     var serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       // Location services are not enabled don't continue
@@ -21,22 +25,31 @@ class GeoPoint {
       return Future.error('Location services are disabled.');
     }
 
-    var permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
+    if (_isRequestingLocationPermissions || _isRequestingLocation) {
+      //To handle the case where a request is already occuring.
+
+      return null;
+    }
+
+    try {
+      _isRequestingLocationPermissions = true;
+      var permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
       }
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+      final pos = await Geolocator.getCurrentPosition();
+      return GeoPoint(pos.latitude, pos.longitude);
+    } catch (e) {
+      return Future.error(e.toString());
     }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-    final pos = await Geolocator.getCurrentPosition();
-
-    return GeoPoint(pos.latitude, pos.longitude);
   }
 
   double distanceTo(GeoPoint other) {
