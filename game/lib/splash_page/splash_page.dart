019e7a3bce67fb912adf14d5455a1c4aeee86082
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-03-28 18:54:46 -0400

    Add images to top left and bottom right corners.

--- game/lib/splash_page/splash_page.dart
+++ game/lib/splash_page/splash_page.dart
@@ -8,56 +8,82 @@ class SplashPageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              LatoText("CornellGo", 60.0, Colors.black, FontWeight.w700),
-              SizedBox(height: 75),
-              LatoText(
-                  "Login to CornellGo", 20.0, Colors.black, FontWeight.w600),
-              SizedBox(height: 32.5),
-              ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll<Color>(Colors.white)),
-                  onPressed: () => {print("pressed")},
-                  child: Container(
-                    width: 255,
-                    height: 53,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: LatoText("Continue with Google", 16.0,
-                          Colors.black, FontWeight.w600),
-                    ),
-                  )),
-              SizedBox(height: 16),
-              Row(children: <Widget>[
-                SizedBox(width: 69),
-                Expanded(child: Divider()),
-                SizedBox(width: 8),
-                LatoText("OR", 14.0, Colors.black, FontWeight.w600),
-                SizedBox(width: 8),
-                Expanded(child: Divider()),
-                SizedBox(width: 69),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            top: -50,
+            height: 200,
+            width: 200,
+            child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/splash_top.png")))),
+          ),
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                LatoText("CornellGo", 60.0, Colors.black, FontWeight.w700),
+                SizedBox(height: 75),
+                LatoText(
+                    "Login to CornellGo", 20.0, Colors.black, FontWeight.w600),
+                SizedBox(height: 32.5),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(Colors.white)),
+                    onPressed: () => {print("pressed")},
+                    child: Container(
+                      width: 255,
+                      height: 53,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: LatoText("Continue with Google", 16.0,
+                            Colors.black, FontWeight.w600),
+                      ),
+                    )),
+                SizedBox(height: 16),
+                Row(children: <Widget>[
+                  SizedBox(width: 69),
+                  Expanded(child: Divider()),
+                  SizedBox(width: 8),
+                  LatoText("OR", 14.0, Colors.black, FontWeight.w600),
+                  SizedBox(width: 8),
+                  Expanded(child: Divider()),
+                  SizedBox(width: 69),
+                ]),
+                SizedBox(height: 16),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(Colors.white)),
+                    onPressed: () => {print("pressed")},
+                    child: Container(
+                      width: 255,
+                      height: 53,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: LatoText("Continue as guest", 16.0, Colors.black,
+                            FontWeight.w600),
+                      ),
+                    ))
               ]),
-              SizedBox(height: 16),
-              ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll<Color>(Colors.white)),
-                  onPressed: () => {print("pressed")},
-                  child: Container(
-                    width: 255,
-                    height: 53,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: LatoText("Continue as guest", 16.0, Colors.black,
-                          FontWeight.w600),
-                    ),
-                  ))
-            ]),
+          Positioned(
+            right: 0,
+            bottom: -60,
+            height: 200,
+            width: 200,
+            child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/splash_bottom.png")))),
+          )
+        ],
       ),
     );
   }
