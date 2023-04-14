8093197472e04dd09b41e200ddedd2d12e135bae
cathli66 <cathrynli606@gmail.com>
2023-04-14 17:00:11 -0400

    added filter button

--- game/lib/challenges/challenges_page.dart
+++ game/lib/challenges/challenges_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'challenge_cell_new.dart';
+import 'package:game/journeys/filter_form.dart';
 
 class ChallengesPage extends StatefulWidget {
   const ChallengesPage({Key? key}) : super(key: key);
@@ -22,6 +23,18 @@ class _ChallengesPageState extends State<ChallengesPage> {
     ChallengeCell("ARTS QUAD", "Statue on the Arts Quad",
         Image.network('https://picsum.photos/250?image=9'), true),
   ];
+
+  void openFilter() {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        builder: (
+          BuildContext context,
+        ) {
+          return FilterForm();
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,33 +64,43 @@ class _ChallengesPageState extends State<ChallengesPage> {
               ),
             ),
             Container(
-              padding: EdgeInsets.only(top: 10, bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.zero,
-                    color: Color.fromARGB(76, 217, 217, 217),
-                    child: TextButton.icon(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.tune,
-                        color: Color.fromARGB(204, 0, 0, 0),
-                        size: 12,
-                      ),
-                      label: Text(
-                        "filter",
-                        style: TextStyle(
-                          color: Color.fromARGB(204, 0, 0, 0),
-                          fontSize: 12,
-                          fontFamily: 'Lato',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 24.0, bottom: 24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 30,
+                      child: TextButton.icon(
+                          onPressed: openFilter,
+                          icon: Icon(
+                            Icons.filter_list_rounded,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            size: 20.0,
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Color.fromARGB(153, 217, 217, 217)),
+                            padding: MaterialStateProperty.all(
+                              EdgeInsets.only(right: 16.0, left: 16.0),
+                            ),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(3.0),
+                            )),
+                          ),
+                          label: Text(
+                            "Filter By",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 15,
+                              fontFamily: 'Inter',
+                            ),
+                          )),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             Expanded(
