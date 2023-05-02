51b726ef52f29f0f956191f2775267bc4583e1b3
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-05-02 18:14:16 -0400

    Register flow (#122)
    * Set up initial screen scaffolds.
    
    * Initial implementation of register page.
    
    * Add images to top left and bottom right corners.
    
    * Build out register page.
    
    * Update register_page.dart
    
    * Finish registration flow FE.
    
    * Update details_page.dart
    
    * Register flow.
    
    * Fix state bug for login request.
    
    * Run formatter.
    
    ---------
    
    Co-authored-by: neketka <neketka@gmail.com>

---
+++ game/lib/register_page/register_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:game/details_page/details_page.dart';
+import 'package:game/widget/lato_text.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class RegisterPageWidget extends StatefulWidget {
+  GoogleSignInAccount? user = null;
+  RegisterPageWidget({Key? key, required GoogleSignInAccount? this.user})
+      : super(key: key);
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  _RegisterPageWidgetState createState() => _RegisterPageWidgetState();
+}
+
+class _RegisterPageWidgetState extends State<RegisterPageWidget> {
+  String _selectedOption = "";
+  @override
+  void initState() {
+    super.initState();
+    _selectedOption = "Graduate Student";
+  }
+
+  final List<String> identityOptions = [
+    "Undergraduate Student",
+    "Graduate Student",
+    "Faculty/Staff",
+    "Alumni"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 50.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  LatoText("Who are you", 24, Colors.black, FontWeight.w700)
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              Column(
+                  children: identityOptions.map((entry) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor: entry == _selectedOption
+                              ? MaterialStatePropertyAll<Color>(Colors.black)
+                              : MaterialStatePropertyAll<Color>(Colors.white)),
+                      onPressed: () => {
+                            setState(() {
+                              _selectedOption = entry;
+                            })
+                          },
+                      child: Container(
+                        width: 255,
+                        height: 53,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: LatoText(
+                              entry,
+                              16.0,
+                              entry != _selectedOption
+                                  ? Colors.black
+                                  : Colors.white,
+                              FontWeight.w600),
+                        ),
+                      )),
+                );
+              }).toList()),
+              SizedBox(
+                height: 100,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.black)),
+                child: Container(
+                    width: 255,
+                    height: 53,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: LatoText(
+                          "Continue", 16.0, Colors.white, FontWeight.w600),
+                    )),
+                onPressed: () => {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => DetailsPageWidget(
+                        userType: _selectedOption,
+                        user: widget.user,
+                      ),
+                    ),
+                  )
+                },
+              ),
+            ]),
+      ),
+    );
+  }
+}
