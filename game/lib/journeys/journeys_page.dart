ba0aa888f3dd2fb466a14cb15618537ab5fcda33
youssef-attia <youssef.attia.ya@gmail.com>
2023-05-04 12:54:03 -0400

    Initial commit

--- game/lib/journeys/journeys_page.dart
+++ game/lib/journeys/journeys_page.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:game/journeys/journey_cell.dart';
 import 'package:game/journeys/filter_form.dart';
+import 'package:game/model/event_model.dart';
+import 'package:game/model/challenge_model.dart';
+import 'package:game/model/group_model.dart';
+import 'package:game/model/tracker_model.dart';
+import 'package:game/model/user_model.dart';
+import 'package:provider/provider.dart';
+import 'package:game/api/game_api.dart';
+import 'package:game/events/event_cell.dart';
+import 'package:game/api/game_client_dto.dart';
+import 'package:game/utils/utility_functions.dart';
+import 'package:intl/intl.dart';
 
 class JourneysPage extends StatefulWidget {
   const JourneysPage({Key? key}) : super(key: key);
@@ -133,14 +144,106 @@ class _JourneysPageState extends State<JourneysPage> {
               ),
             ),
             Expanded(
-              child: ListView.separated(
-                padding: const EdgeInsets.all(0),
-                itemCount: cells.length,
-                itemBuilder: (context, index) {
-                  return cells[index];
-                },
-                separatorBuilder: (context, index) {
-                  return SizedBox(height: 10);
+              child: Consumer5<EventModel, GroupModel, TrackerModel,
+                  ChallengeModel, UserModel>(
+                builder: (context, myEventModel, groupModel, trackerModel,
+                    challengeModel, userModel, child) {
+                  List<Widget> eventCells = [];
+                  if (myEventModel.searchResults == null) {
+                    myEventModel.searchEvents(
+                        0,
+                        1000,
+                        [
+                          EventRewardType.PERPETUAL,
+                          EventRewardType.LIMITED_TIME_EVENT
+                        ],
+                        false,
+                        false,
+                        false);
+                  }
+                  final events = myEventModel.searchResults ?? [];
+                  if (!events
+                      .any((element) => element.id == groupModel.curEventId)) {
+                    final curEvent =
+                        myEventModel.getEventById(groupModel.curEventId ?? "");
+                    if (curEvent != null) events.add(curEvent);
+                  }
+                  for (EventDto event in events) {
+                    final reward =
+                        event.rewardIds.length == 0 ? null : event.rewardIds[0];
+                    final tracker = trackerModel.trackerByEventId(event.id);
+                    final format = DateFormat('yyyy-MM-dd');
+                    final chal = event.challengeIds.length == 0
+                        ? null
+                        : challengeModel
+                            .getChallengeById(event.challengeIds[0]);
+                    final complete = tracker?.prevChallengeIds.length ==
+                        event.challengeIds.length;
+                    final timeTillExpire = DateTime.parse(event.endTime)
+                        .difference(DateTime.now());
+                    eventCells.add(
+                      GestureDetector(
+                        onTap: () {
+                          if (groupModel.curEventId == event.id) return;
+                          if (groupModel.members.any((element) =>
+                              element.id == userModel.userData?.id &&
+                              element.id == groupModel.group!.hostId)) {
+                            // _showConfirmation(context, event.id, event.name);
+                          } else {
+                            showAlert(
+                                "Ask the group leader to change the event.",
+                                context);
+                          }
+                        },
+                        child: StreamBuilder(
+                          stream:
+                              Stream.fromFuture(Future.delayed(timeTillExpire)),
+                          builder: (stream, value) => timeTillExpire.isNegative
+                              ? Consumer<ApiClient>(
+                                  builder: (context, apiClient, child) {
+                                    if (event.id == groupModel.curEventId) {
+                                      apiClient.serverApi?.setCurrentEvent("");
+                                    }
+                                    return Container();
+                                  },
+                                )
+                              : EventCell(
+                                  event.name,
+                                  format.format(DateTime.parse(event.endTime)),
+                                  event.description,
+                                  complete,
+                                  event.id == groupModel.curEventId,
+                                  DateTime.parse(event.endTime),
+                                  reward ?? "",
+                                  event.rewardIds.length,
+                                  event.requiredMembers,
+                                  chal?.imageUrl ??
+                                      "https://a.rgbimg.com/users/b/ba/badk/600/qfOGvbS.jpg",
+                                ),
+                        ),
+                      ),
+                    );
+                  }
+                  return ListView.separated(
+                    padding: const EdgeInsets.all(0),
+                    itemCount: eventCells.length,
+                    itemBuilder: (context, index) {
+                      return eventCells[index];
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(height: 10);
+                    },
+                  );
+                  // return ListView.separated(
+                  //   padding: const EdgeInsets.all(0),
+                  //   itemCount: cells.length,
+                  //   itemBuilder: (context, index) {
+                  //     return cells[index];
+                  //   },
+                  //   separatorBuilder: (context, index) {
+                  //     return SizedBox(height: 10);
+                  //   },
+                  // );
                 },
               ),
             ),
