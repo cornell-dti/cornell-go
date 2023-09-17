782c5d911f7df998ac53b0b8e3d52b7be5eb6438
neketka <neketka@gmail.com>
2023-09-17 14:10:44 -0400

    Removed shut down hooks from prisma

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -7,9 +7,11 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
+  /* 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close();
     });
-  }
+  } 
+  */
 }
