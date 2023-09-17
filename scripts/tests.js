c763247389d9f3586ac31cd5e667c866a85764d6
neketka <neketka@gmail.com>
2023-09-17 03:36:52 -0400

    Fixed testing script

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,5 +1,17 @@
 const { execSync } = require("child_process");
-const { rmdirSync, existsSync } = require("fs");
+const { rmSync, existsSync, mkdirSync, readdirSync, lstatSync, copyFileSync } = require("fs");
+const path = require("path");
+
+function copyFolderSync(from, to) { // https://stackoverflow.com/questions/13786160/copy-folder-recursively-in-node-js
+  mkdirSync(to);
+  readdirSync(from).forEach(element => {
+    if (lstatSync(path.join(from, element)).isFile()) {
+      copyFileSync(path.join(from, element), path.join(to, element));
+    } else {
+      copyFolderSync(path.join(from, element), path.join(to, element));
+    }
+  });
+}
 
 async function main() {
   if (process.argv.length < 3 || !(["unit", "e2e"].includes(process.argv[2]))) {
@@ -10,18 +22,27 @@ async function main() {
   const testType = process.argv[2].toUpperCase();
   const saveOldPostgres = existsSync("./postgres-data");
 
+  if (existsSync("./postgres-data-saved")) {
+    rmSync("./postgres-data-saved", { recursive: true, force: true });
+  }
+
   if (saveOldPostgres) {
-    execSync("npx copyfiles ./postgres-data ./postgres-data-saved");
+    copyFolderSync("./postgres-data", "./postgres-data-saved");
+    console.log("Backed up old db data");
   }
 
   try {
     execSync("npm run dbreset");
-    execSync(`set TESTING_${testType}=true docker compose up --build --no-attach postgres`);
+    console.log("Set up db for testing");
+    process.env[`TESTING_${testType}`] = "true";
+    execSync(`docker compose up --build --no-attach postgres`);
   } finally {
-    //execSync("docker compose stop");
+    execSync("docker compose stop");
     if (saveOldPostgres) {
-      //rmdirSync("./postgres-data");
-      execSync("npx copyfiles ./postgres-data-saved ./postgres-data");
+      rmSync("./postgres-data", { recursive: true, force: true });
+      copyFolderSync("./postgres-data-saved", "./postgres-data");
+      rmSync("./postgres-data-saved", { recursive: true, force: true });
+      console.log("Restored old db data");
     }
   }
 }
