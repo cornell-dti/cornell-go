7b68140a3f459f8b76a947c04b837f528ffb2334
Chris Gu <54943842+cgu2020@users.noreply.github.com>
2023-11-01 17:38:44 -0400

    Bottom Navigation Bar (#138)
    * creaateed changes  on the ui
    
    * ad phots
    
    * pass it off the youseef
    
    * centered text
    
    * install flutter map
    
    * got the map working
    
    * e2e spec
    
    * swap to gameplaypage
    
    * Imported Icons and created bottom navbar
    
    * Navbar with svg icons implemented
    
    * Navigation Bar finished
    
    * podfile
    
    * Fixed all dependency issues and upgraded packages
    
    ---------
    
    Co-authored-by: Eric Huang <ceh256@cornell.edu>

---
+++ game/lib/navbar.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:game/gameplay/gameplay_page.dart';
+import 'package:game/global_leaderboard/global_leaderboard_widget.dart';
+import 'package:game/journeys/journeys_page.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    JourneysPage(),
+    Text(
+      'Leaderboard',
+      style: optionStyle,
+    ),
+    Text(
+      'Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icons/home.svg",
+                colorFilter: ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
+            activeIcon: SvgPicture.asset("assets/icons/home.svg",
+                colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn)),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icons/leaderboard.svg",
+                colorFilter: ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
+            activeIcon: SvgPicture.asset("assets/icons/leaderboard.svg",
+                colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn)),
+            label: 'Leaderboard',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icons/profilehead.svg",
+                colorFilter: ColorFilter.mode(Colors.grey, BlendMode.srcIn)),
+            activeIcon: SvgPicture.asset("assets/icons/profilehead.svg",
+                colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn)),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
