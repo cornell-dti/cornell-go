ba68a8fedb6fa5af41ab431a0a3d1e7890b36a3f
Chris Gu <chrisgu12321@gmail.com>
2023-11-30 17:37:02 -0500

    Fixed path causing build to fail

--- game/lib/details_page/details_page.dart
+++ game/lib/details_page/details_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:game/home_page/home_page_widget.dart';
 import 'package:game/main.dart';
-import 'package:game/navbar.dart';
+import 'package:game/navigation_page/bottom_navbar.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:game/utils/utility_functions.dart';
 
