5e4ddc169f4f6ff09046d001f1868f79003c7a5c
cl893 <cl893@cornell.edu>
2024-02-18 23:16:01 -0500

    second attempt at integrating gmaps api into android, without upgrading gradle

--- game/lib/gameplay/gameplay_map.dart
+++ game/lib/gameplay/gameplay_map.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+// import 'package:flutter_map/flutter_map.dart';
+// import 'package:latlong2/latlong.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
 class GameplayMap extends StatefulWidget {
   const GameplayMap({Key? key}) : super(key: key);
@@ -12,20 +15,57 @@ class GameplayMap extends StatefulWidget {
 }
 
 class _GameplayMapState extends State<GameplayMap> {
-  final mapController = MapController();
+  // final mapController = MapController();
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-33.86, 151.20);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(51.509364, -0.128928),
-        zoom: 9.2,
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
       ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
         ),
-      ],
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+          // markers: {
+          //   const Marker(
+          //     markerId: const MarkerId("Sydney"),
+          //     position: LatLng(-33.86, 151.20),
+          //     infoWindow: InfoWindow(
+          //       title: "Sydney",
+          //       snippet: "Capital of New South Wales",
+          //     ),
+          //   ),
+          // },
+        ),
+      ),
     );
+    // return FlutterMap(
+    //   options: MapOptions(
+    //     center: LatLng(51.509364, -0.128928),
+    //     zoom: 9.2,
+    //   ),
+    //   children: [
+    //     TileLayer(
+    //       urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    //       userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+    //     ),
+    //   ],
+    // );
   }
 }
