02efcb17424b45e0b5e7afe28324d840cc864153
neketka <neketka@gmail.com>
2024-02-22 18:57:20 -0500

    Create claims schema

--- server/src/claim/claim.service.ts
+++ server/src/claim/claim.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { ClaimedResourceType, User, ClaimType } from '@prisma/client';
 
 @Injectable()
-export class ClaimService {}
+export class ClaimService {
+  constructor(private prisma: PrismaService) {}
+
+  async modifyClaims(
+    claimType: ClaimType,
+    issuer: User | null,
+    user: User | null,
+    resourceType: ClaimedResourceType | null,
+    resources: string[] | null,
+    propertyNames: string[] | null,
+  ) {}
+}
