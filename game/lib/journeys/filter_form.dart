fbb7277d0155d01d9a00fb7f8f977079d5a3cfe6
Nitya <nityapakala@gmail.com>
2024-03-06 21:34:47 -0600

    updated ui and cancel button
    updated ui and cancel button

--- game/lib/journeys/filter_form.dart
+++ game/lib/journeys/filter_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:game/journeys/journeys_page.dart';
 
 class FilterForm extends StatefulWidget {
   const FilterForm({Key? key}) : super(key: key);
@@ -54,179 +55,186 @@ class _FilterFormState extends State<FilterForm> {
     return FractionallySizedBox(
       heightFactor: 0.9,
       child: Container(
-        margin: EdgeInsets.all(32.0),
         child: ListView(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Filters',
-                  style: TextStyle(
-                    color: Color.fromARGB(127, 0, 0, 0),
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins',
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              color: Color.fromARGB(255, 237, 86, 86),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 161.0),
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Filters',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22,
+                              // fontWeight: FontWeight.bold,
+                              fontFamily: 'Poppins',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 110),
+                        child: SizedBox(
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: CloseButton(color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.close, size: 20.0),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        Color.fromARGB(153, 217, 217, 217)),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-            ListTile(
-              title: Text('Category',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins',
-                  )),
-              contentPadding: EdgeInsets.zero,
-            ),
-            Wrap(
-              spacing: 4,
-              runSpacing: 4, // vertical spacing
-              children: categories.map((category) {
-                return ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      toggleCategory(category);
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: selectedCategories.contains(category)
-                        ? Color.fromARGB(255, 249, 237, 218)
-                        : Color.fromARGB(100, 210, 210, 210),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    elevation: 0, // Set elevation to 0 to remove drop shadow
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        // horizontal: 2.0,
-                        vertical: 8.0),
-                    child: Text(
-                      category,
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    title: Text(
+                      'Category',
                       style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.black.withOpacity(0.6),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
                       ),
                     ),
+                    contentPadding: EdgeInsets.zero,
                   ),
-                );
-              }).toList(),
-            ),
-            ListTile(
-              title: Text('Location',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins',
-                  )),
-              contentPadding: EdgeInsets.zero,
-            ),
-            Wrap(
-              spacing: 4,
-              runSpacing: 4, // vertical spacing
-              children: locations.map((location) {
-                return ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      toggleLocation(location);
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: selectedLocations.contains(location)
-                        ? Color.fromARGB(255, 249, 237, 218)
-                        : Color.fromARGB(100, 210, 210, 210),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    elevation: 0, // Set elevation to 0 to remove drop shadow
+                  Wrap(
+                    spacing: 4,
+                    runSpacing: 4,
+                    children: categories.map((category) {
+                      return ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            toggleCategory(category);
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: selectedCategories.contains(category)
+                              ? Color.fromARGB(255, 249, 237, 218)
+                              : Color.fromARGB(100, 210, 210, 210),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          elevation: 0,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                          child: Text(
+                            category,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black.withOpacity(0.6),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        // horizontal: 2.0,
-                        vertical: 8.0),
-                    child: Text(
-                      location,
+                  ListTile(
+                    title: Text(
+                      'Location',
                       style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.black.withOpacity(0.6),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
                       ),
                     ),
+                    contentPadding: EdgeInsets.zero,
                   ),
-                );
-              }).toList(),
-            ),
-            ListTile(
-              title: Text('Difficulty Levels',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins',
-                  )),
-              contentPadding: EdgeInsets.zero,
-            ),
-            // Column(
-            //   children: statuses
-            //       .map((status) => RadioListTile(
-            //             title: Text(status, style: TextStyle(fontSize: 13.0)),
-            //             value: status,
-            //             groupValue: selectedStatus,
-            //             onChanged: (changedValue) {
-            //               setState(() {
-            //                 setStatus(status);
-            //               });
-            //             },
-            //             contentPadding: EdgeInsets.all(0),
-            //           ))
-            //       .toList(),
-            // ),
-
-            Wrap(
-              spacing: 4,
-              runSpacing: 4, // vertical spacing
-              children: statuses.map((status) {
-                return ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      setStatus(status);
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: selectedStatus.contains(status)
-                        ? Color.fromARGB(255, 249, 237, 218)
-                        : Color.fromARGB(100, 210, 210, 210),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    elevation: 0, // Set elevation to 0 to remove drop shadow
+                  Wrap(
+                    spacing: 4,
+                    runSpacing: 4,
+                    children: locations.map((location) {
+                      return ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            toggleLocation(location);
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: selectedLocations.contains(location)
+                              ? Color.fromARGB(255, 249, 237, 218)
+                              : Color.fromARGB(100, 210, 210, 210),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          elevation: 0,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                          child: Text(
+                            location,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black.withOpacity(0.6),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        // horizontal: 2.0,
-                        vertical: 8.0),
-                    child: Text(
-                      status,
+                  ListTile(
+                    title: Text(
+                      'Difficulty Levels',
                       style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.black.withOpacity(0.6),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
                       ),
                     ),
+                    contentPadding: EdgeInsets.zero,
                   ),
-                );
-              }).toList(),
+                  Wrap(
+                    spacing: 4,
+                    runSpacing: 4,
+                    children: statuses.map((status) {
+                      return ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            setStatus(status);
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: selectedStatus.contains(status)
+                              ? Color.fromARGB(255, 249, 237, 218)
+                              : Color.fromARGB(100, 210, 210, 210),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          elevation: 0,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8.0),
+                          child: Text(
+                            status,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black.withOpacity(0.6),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
             ),
             Padding(padding: EdgeInsets.symmetric(vertical: 40.0)),
-
             Container(
               width: 600,
               decoration: ShapeDecoration(
@@ -240,41 +248,46 @@ class _FilterFormState extends State<FilterForm> {
               ),
             ),
             Padding(padding: EdgeInsets.symmetric(vertical: 20.0)),
-
             Row(
               // padding: EdgeInsets.symmetric(vertical: 30),
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                TextButton(
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedCategories = [];
+                          selectedLocations = [];
+                          selectedStatus = 'All';
+                        });
+                      },
+                      child: Text('Clear'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                        foregroundColor: Color.fromARGB(255, 0, 0, 0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          // side: BorderSide(color: Colors.black, width: 1.0),
+                        ),
+                      )),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextButton(
                     onPressed: () {
-                      setState(() {
-                        selectedCategories = [];
-                        selectedLocations = [];
-                        selectedStatus = 'All';
-                      });
+                      // TODO: Implement apply filters button
                     },
-                    child: Text('Clear'),
+                    child: Text('See results'),
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                      foregroundColor: Color.fromARGB(255, 0, 0, 0),
+                      foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                      backgroundColor: Color(0xFFEC5555),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10.0),
-                        // side: BorderSide(color: Colors.black, width: 1.0),
                       ),
-                    )),
-                TextButton(
-                  onPressed: () {
-                    // TODO: Implement apply filters button
-                  },
-                  child: Text('See results'),
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Color.fromARGB(255, 255, 255, 255),
-                    backgroundColor: Color(0xFFEC5555),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
                     ),
                   ),
-                ),
+                )
               ],
             ),
           ],
