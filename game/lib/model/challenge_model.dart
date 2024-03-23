8dd479cb34f6326b5f2346cc813003c13db04769
Brian La <brianyla@gmail.com>
2024-03-23 15:20:26 -0400

    Removed dummy data + added comments

--- game/lib/model/challenge_model.dart
+++ game/lib/model/challenge_model.dart
@@ -2,11 +2,17 @@ import 'package:flutter/foundation.dart';
 import 'package:game/api/game_api.dart';
 import 'package:game/api/game_client_dto.dart';
 
+/**
+ * This file represents the model for the challenges. Whenever a challenge is updated, added or deleted from the backend, the model is updated and notifies the Consumer so that the front end can be modified. 
+ */
 class ChallengeModel extends ChangeNotifier {
   Map<String, ChallengeDto> _challengesById = {};
   ApiClient _client;
 
   ChallengeModel(ApiClient client) : _client = client {
+    /**
+     * Stream that listens to updates on the challenges. Challenges can be either deleted or updated. 
+     */
     client.clientApi.updateChallengeDataStream.listen((event) {
       if (event.challengeId != "") {
         //event.challengeId!=null => event to be deleted.
@@ -30,6 +36,11 @@ class ChallengeModel extends ChangeNotifier {
     });
   }
 
+  /**
+   * id: The id of the challenge to be retrieved.
+   * 
+   * If the id of the challenge is not found in the model, the client requests the challenge data from the server. When the data is retrieved, the challengeModel will listen and update its information, notifying the front end through Consumers. If found, it will return the challenge.
+   */
   ChallengeDto? getChallengeById(String id) {
     final chal = _challengesById[id];
     if (chal == null) {
