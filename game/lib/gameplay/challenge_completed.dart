2cbbb913136b6de316b25e4e017134fdaf65ac4d
Chris Gu <chrisgu12321@gmail.com>
2024-04-13 16:11:18 -0400

    completing challenge page work

--- game/lib/gameplay/challenge_completed.dart
+++ game/lib/gameplay/challenge_completed.dart
@@ -9,16 +9,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class ChallengeCompleted extends StatefulWidget {
-  final GeoPoint targetLocation;
-  final double awardingRadius;
   final String description;
   final int points;
   final int numHintsLeft;
 
   const ChallengeCompleted(
       {Key? key,
-      required this.targetLocation,
-      required this.awardingRadius,
       required this.description,
       required this.points,
       required this.numHintsLeft})
@@ -39,12 +35,67 @@ class _ChallengeCompletedState extends State<ChallengeCompleted> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      height: MediaQuery.of(context).size.height,
-      child: SvgPicture.asset(
-        'assets/image/challenge-completed-bg.svg', // Replace with your SVG file path
-        fit: BoxFit.cover,
-      ),
-    ));
+        body: Stack(children: [
+      Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: SvgPicture.asset(
+            'assets/images/challenge-completed-bg.svg', // Replace with your SVG file path
+            fit: BoxFit.cover,
+          )),
+      Container(
+          margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.5,
+              left: 20,
+              right: 20),
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Column(children: [
+            Container(
+              padding: EdgeInsets.only(bottom: 12),
+              child: Text(
+                'Challenge Complete!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: 30),
+              child: Text(
+                'You’ve found the Statue on the Arts Quad!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 30, bottom: 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Points',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 30, bottom: 50),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Points',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ]))
+    ]));
   }
 }
