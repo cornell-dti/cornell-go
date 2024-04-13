fa1719a12359a00edae489d18b2ccd181fc830ec
neketka <neketka@gmail.com>
2024-04-12 22:00:50 -0400

    Reordered ifs inside start.sh

--- server/start.sh
+++ server/start.sh
@@ -3,14 +3,14 @@
 npx prisma migrate deploy || true
 node src/caslfix.js
 
-if [ "$DEVELOPMENT" = "false" ]; then 
-  npm run start:prod;
-elif [ "$DB_RESET" = "true" ]; then
+if [ "$DB_RESET" = "true" ]; then
   npx prisma db push --accept-data-loss --force-reset
 elif [ "$TESTING_UNIT" = "true" ]; then 
   npm run test;  
 elif [ "$TESTING_E2E" = "true" ]; then 
   npm run test:e2e;
+elif [ "$DEVELOPMENT" = "false" ]; then 
+  npm run start:prod;
 else 
   npm run start;
 fi
