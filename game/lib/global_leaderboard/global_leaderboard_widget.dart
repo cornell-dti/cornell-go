eb50d35b2e67356a6fc607025c7d4474a8c811e4
cathli66 <lifamily4@hotmail.com>
2024-04-28 02:54:00 -0400

    consolidated consumers

--- game/lib/global_leaderboard/global_leaderboard_widget.dart
+++ game/lib/global_leaderboard/global_leaderboard_widget.dart
@@ -45,159 +45,148 @@ class _GlobalLeaderboardWidgetState extends State<GlobalLeaderboardWidget> {
     var scoreList = [];
 
     return Scaffold(
-        key: scaffoldKey,
-        backgroundColor: Color.fromARGB(255, 255, 248, 241),
-        appBar: AppBar(
-          toolbarHeight: 85,
-          automaticallyImplyLeading: false,
-          backgroundColor: Color.fromARGB(255, 237, 86, 86),
-          flexibleSpace: FlexibleSpaceBar(
-              title: Padding(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Leaderboard',
-                  style: leaderboardStyle,
-                ),
+      key: scaffoldKey,
+      backgroundColor: Color.fromARGB(255, 255, 248, 241),
+      appBar: AppBar(
+        toolbarHeight: 85,
+        automaticallyImplyLeading: false,
+        backgroundColor: Color.fromARGB(255, 237, 86, 86),
+        flexibleSpace: FlexibleSpaceBar(
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Text(
+                'Leaderboard',
+                style: leaderboardStyle,
               ),
-              centerTitle: true),
-          actions: [],
-        ),
-        body: Center(
+            ),
+            centerTitle: true),
+        actions: [],
+      ),
+      body: Consumer3<GroupModel, EventModel, UserModel>(
+          builder: (context, myGroupModel, myEventModel, myUserModel, child) {
+        //Loading in the lists and then creating podiumList of top 3
+        final List<LeaderDto> list =
+            myEventModel.getTopPlayersForEvent('', 1000);
+
+        list.sort((a, b) => b.score.compareTo(a.score));
+        LeaderDto empty = LeaderDto(
+          userId: " ",
+          username: " ",
+          score: 0,
+        );
+
+        // Creating list to be displayed within the podium (filled with empty users if lists length is less than 3)
+        List<LeaderDto> fullList = List.from(list);
+
+        int iterTillFull = 3 - fullList.length;
+        if (fullList.length < 3) {
+          for (int i = 0; i < iterTillFull; i++) {
+            fullList.add(empty);
+          }
+        }
+        // Leaderboard starts at 4th position because first three already in podium
+        int position = 4;
+
+        List<LeaderDto> podiumList = fullList.sublist(0, 3);
+
+        bool firstPodiumUser = podiumList.length > 0 &&
+            podiumList[0].userId == myUserModel.userData?.id;
+
+        bool secondPodiumUser = podiumList.length > 1 &&
+            podiumList[1].userId == myUserModel.userData?.id;
+
+        bool thirdPodiumUser = podiumList.length > 2 &&
+            podiumList[2].userId == myUserModel.userData?.id;
+
+        return Center(
+            //Podium Container
             child: Column(children: [
-          //Podium Container
-          Consumer3<GroupModel, EventModel, UserModel>(builder:
-              (context, myGroupModel, myEventModel, myUserModel, child) {
-            //Loading in the lists and then creating podiumList of top 3
-            final List<LeaderDto> list =
-                myEventModel.getTopPlayersForEvent('', 1000);
-
-            list.sort((a, b) => b.score.compareTo(a.score));
-            LeaderDto empty = LeaderDto(
-              userId: " ",
-              username: " ",
-              score: 0,
-            );
-
-            // Creating list to be displayed within the podium (filled with empty users if lists length is less than 3)
-            List<LeaderDto> fullList = List.from(list);
-
-            int iterTillFull = 3 - fullList.length;
-            if (fullList.length < 3) {
-              for (int i = 0; i < iterTillFull; i++) {
-                fullList.add(empty);
-              }
-            }
-
-            List<LeaderDto> podiumList = fullList.sublist(0, 3);
-
-            bool firstPodiumUser = podiumList.length > 0 &&
-                podiumList[0].userId == myUserModel.userData?.id;
-
-            bool secondPodiumUser = podiumList.length > 1 &&
-                podiumList[1].userId == myUserModel.userData?.id;
-
-            bool thirdPodiumUser = podiumList.length > 2 &&
-                podiumList[2].userId == myUserModel.userData?.id;
-
-            return Padding(
-              padding:
-                  EdgeInsets.only(top: MediaQuery.sizeOf(context).width * 0.05),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-                      podiumList.length > 1
-                          ? podiumCell(
-                              context, podiumList[1].username, secondPodiumUser)
-                          : podiumCell(context, "", false),
-                      SecondPodium(
-                          context, podiumList[1].score, secondPodiumUser)
-                    ]),
-                    SizedBox(width: MediaQuery.sizeOf(context).width * 0.03),
-                    Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-                      podiumList.length > 0
-                          ? podiumCell(
-                              context, podiumList[0].username, firstPodiumUser)
-                          : podiumCell(context, "", false),
-                      FirstPodium(context, podiumList[0].score, firstPodiumUser)
-                    ]),
-                    SizedBox(width: MediaQuery.sizeOf(context).width * 0.03),
-                    Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-                      podiumList.length > 2
-                          ? podiumCell(
-                              context, podiumList[2].username, thirdPodiumUser)
-                          : podiumCell(context, "", false),
-                      ThirdPodium(context, podiumList[2].score, thirdPodiumUser)
-                    ]),
+          Padding(
+            padding:
+                EdgeInsets.only(top: MediaQuery.sizeOf(context).width * 0.05),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    podiumList.length > 1
+                        ? podiumCell(
+                            context, podiumList[1].username, secondPodiumUser)
+                        : podiumCell(context, "", false),
+                    SecondPodium(context, podiumList[1].score, secondPodiumUser)
                   ]),
-            );
-          }),
+                  SizedBox(width: MediaQuery.sizeOf(context).width * 0.03),
+                  Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    podiumList.length > 0
+                        ? podiumCell(
+                            context, podiumList[0].username, firstPodiumUser)
+                        : podiumCell(context, "", false),
+                    FirstPodium(context, podiumList[0].score, firstPodiumUser)
+                  ]),
+                  SizedBox(width: MediaQuery.sizeOf(context).width * 0.03),
+                  Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    podiumList.length > 2
+                        ? podiumCell(
+                            context, podiumList[2].username, thirdPodiumUser)
+                        : podiumCell(context, "", false),
+                    ThirdPodium(context, podiumList[2].score, thirdPodiumUser)
+                  ]),
+                ]),
+          ),
           SizedBox(height: MediaQuery.sizeOf(context).height * 0.01),
           Expanded(
-            child: Consumer3<GroupModel, EventModel, UserModel>(
-              builder:
-                  (context, myGroupModel, myEventModel, myUserModel, child) {
-                // Use this line below to retrieve actual data
-                final List<LeaderDto> list =
-                    myEventModel.getTopPlayersForEvent('', 1000);
-                // Leaderboard starts at 4th position because first three already in podium
-                int position = 4;
-
-                list.sort((a, b) => b.score.compareTo(a.score));
-
-                return Container(
-                  width: 360.0,
-                  height: 446.0,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(255, 170, 91, 0.15),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10.0),
-                      topRight: Radius.circular(10.0),
-                    ),
-                  ),
-                  child: ListView(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.vertical,
-                    children: [
-                      for (LeaderDto user in list.skip(3))
-                        Align(
-                          alignment: Alignment.center,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 16),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10.0),
-                                  topRight: Radius.circular(10.0),
-                                  bottomLeft: Radius.circular(10.0),
-                                  bottomRight: Radius.circular(10.0),
-                                ),
-                                color: Colors.white,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Color(0x40000000),
-                                    offset: Offset(0.0, 1.745),
-                                    blurRadius: 6.989011764526367,
-                                  ),
-                                ],
-                              ),
-                              child: leaderBoardCell(
-                                context,
-                                user.username,
-                                position++,
-                                user.score,
-                                user.userId == myUserModel.userData?.id,
-                              ),
+            child: Container(
+              width: 360.0,
+              height: 446.0,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(255, 170, 91, 0.15),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0),
+                ),
+              ),
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                children: [
+                  for (LeaderDto user in list.skip(3))
+                    Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 16),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10.0),
+                              topRight: Radius.circular(10.0),
+                              bottomLeft: Radius.circular(10.0),
+                              bottomRight: Radius.circular(10.0),
                             ),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0x40000000),
+                                offset: Offset(0.0, 1.745),
+                                blurRadius: 6.989011764526367,
+                              ),
+                            ],
+                          ),
+                          child: leaderBoardCell(
+                            context,
+                            user.username,
+                            position++,
+                            user.score,
+                            user.userId == myUserModel.userData?.id,
                           ),
                         ),
-                    ],
-                  ),
-                );
-              },
+                      ),
+                    ),
+                ],
+              ),
             ),
           )
-        ])));
+        ]));
+      }),
+    );
   }
 }
