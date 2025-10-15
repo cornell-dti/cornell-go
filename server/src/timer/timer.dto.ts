b8eebf5eb51ae55b4a95b5b7afdee2c239cc6ea6
ambers7 <ambrshen@gmail.com>
2025-10-15 19:21:17 -0400

    Timer DTOs

---
+++ server/src/timer/timer.dto.ts
@@ -0,0 +1,35 @@
+import { ChallengeDto } from "../challenge/challenge.dto";
+
+/**
+ * DTOs for Timer Lifecycle events: TimerStartedDto, TimerExtendedDto, TimerCompletedDto
+ * DTOs for Timer Control Requests: ExtendTimerDto, StartChallengeTimerDto
+ * DTOs for Warning Notifications: TimerWarningDto
+ */
+export interface TimerStartedDto {
+    endTime: string;
+    challenge: ChallengeDto;
+}
+export interface TimerExtendedDto {
+    challengeId: string;
+    newEndTime: string;
+}
+export interface TimerCompletedDto {
+    challengeId: string;
+    challengeCompleted: boolean; //true if completed, false if failed 
+}
+
+export interface ExtendTimerDto {
+    challengeId: string;
+    endTime: string;
+}
+
+export interface StartChallengeTimerDto {
+    challengeId: string;
+}
+
+export interface TimerWarningDto {
+    challengeId: string;
+    milestone: number; //milestone (300s passed, 60s passed, 30s passed)
+    timeRemaining: number; //seconds remaining
+}
+
