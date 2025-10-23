4f26b5395d84f62bfda7d7f4d30fece9413a982e
jiminkim214 <jiminkim214@gmail.com>
2025-10-23 12:40:06 -0400

    created quiz modules

---
+++ server/src/quiz/quiz.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { QuizService } from './quiz.service';
+import { QuizGateway } from './quiz.gateway';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Module({
+  providers: [
+    QuizService,
+    QuizGateway,
+    PrismaService,
+  ],
+  exports: [QuizService],
+})
+export class QuizModule {}
