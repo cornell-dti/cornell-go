594a631fcc3368ccb4ac4eb3797e6750e595d1a6
Jimin Kim <jiminkim214@gmail.com>
2025-12-04 02:12:21 -0500

    Add QuizModel to main.dart and updated QuizModule

--- server/src/quiz/quiz.module.ts
+++ server/src/quiz/quiz.module.ts
@@ -3,10 +3,16 @@ import { QuizService } from './quiz.service';
 import { QuizGateway } from './quiz.gateway';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthModule } from '../auth/auth.module';
+import { EventModule } from '../event/event.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [
+    forwardRef(() => AuthModule),
+    forwardRef(() => EventModule),
+    forwardRef(() => UserModule),
+  ],
   providers: [QuizService, QuizGateway, PrismaService],
   exports: [QuizService],
 })
-export class QuizModule {}
+export class QuizModule { }
