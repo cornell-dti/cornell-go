28dcd1a589e1f6fdfe5bcd3f1a045a4cf14079de
Temi Adebowale <65270955+3TTemi@users.noreply.github.com>
2026-01-09 19:44:23 -0600

    Ta375/fix server ci tests (#288)
    * add fake change
    
    * Add broken workflow to all pushes
    
    * Fix CI to show actual Docker build errors
    
    * Add skipLibCheck to fix dependency type errors in CI
    
    * Clean up CI scripts, remove verbose debug flags
    
    * Restore comment fake change
    
    * Change workflow to run only on PR's

--- scripts/dbreset.js
+++ scripts/dbreset.js
@@ -16,7 +16,8 @@ async function main() {
   process.env[`DB_RESET`] = "true";
   console.log("Setting up database through docker");
   execSync(
-    "docker compose up --build --no-attach postgres --exit-code-from server"
+    "docker compose up --build --no-attach postgres --exit-code-from server",
+    { stdio: "inherit" }
   );
   chdir("./server");
   console.log("Installing server dependencies");
