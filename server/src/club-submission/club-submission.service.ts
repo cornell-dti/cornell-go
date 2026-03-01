034484cf1b31d8f9bd86542ba68b54ca644d3b15
ambers7 <ambrshen@gmail.com>
2026-03-01 14:38:22 -0500

    Created service method to make a CampusEvent

--- server/src/club-submission/club-submission.service.ts
+++ server/src/club-submission/club-submission.service.ts
@@ -7,4 +7,46 @@ import { Injectable } from '@nestjs/common';
  * 
  */
 @Injectable()
-export class ClubSubmissionService {}
+export class ClubSubmissionService {
+    constructor(
+        private  prisma: PrismaService
+    ){}
+
+    /**
+     * Creates a CampusEvent from ClubSubmission Google Form data
+     * sets approvalStatus to PENDING and EventSource to COMMUNITY_SUBMITTED
+     */
+    async createClubSubmission(dto: ClubSubmissionDto) {
+        const campusEvent: CampusEvent =  this.prisma.campusEvent.create({
+            data: {
+                title: dto.eventTitle,
+                description: dto.description,
+                imageUrl: dto.imageUrl ?? null, 
+
+                startTime: new Date(dto.startTime),
+                endTime: new Date(dto.endTime),
+
+                locationName: dto.location,
+                address: dto.address ?? null,
+                latitude: dto.latitude ?? null, // TODO: create default lat/long
+                longitude: dto.longtitude ?? null,
+
+                category: [dto.category],
+                tags: [],
+
+                organizerName: dto.clubName,
+                organizerEmail: dto.contactEmail,
+                registrationUrl: dto.registrationLink ?? null,
+
+                approvalStatus: ApprovalStatus.PENDING,
+                source: EventSource.COMMUNITY_SUBMITTED
+
+            }
+        }
+
+        )
+        console.log(`CampusEvent created`);
+    
+        return campusEvent
+    }
+}
